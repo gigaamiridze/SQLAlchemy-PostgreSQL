539633b82b380aed1b8c79f10d70bc3354afd6c1
Giga Amiridze <flameyungoxx@gmail.com>
2022-10-26 17:31:13 +0400

    Added initiated extensions

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask, Markup
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
+    register_extensions(app)
 
     @app.route("/")
     def home():
         return Markup("<h1>Welcome, How cool is this?</h1>")
 
     return app
+
+def register_extensions(app):
+    # Setup Flask-SQLAlchemy
+    db.init_app(app)
+
+    # Setup Flask-Migrate
+    migrate.init_app(app, db)
